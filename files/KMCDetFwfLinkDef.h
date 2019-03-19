f0858ffc7aac245644f194efc036d68c0b7750cf
shahoian <ruben.shahoyan@cern.ch>
2019-03-19 15:25:33 +0100

    Makefile added for single lib compilation:
    cd files;
    make
    This will create libKMCDetFwd.so library which can be loaded to the aliroot (instead of running load.C)
    via gSystem->Load(libKMCDetFwd.so)

---
+++ files/KMCDetFwfLinkDef.h
@@ -0,0 +1,19 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class GenMUONLMR+;
+#pragma link C++ class KMCClusterFwd+;
+#pragma link C++ class KMCDetectorFwd+;
+#pragma link C++ class KMCFlukaParser+;
+#pragma link C++ class KMCLayerFwd+;
+#pragma link C++ class KMCProbeFwd+;
+#pragma link C++ class NaCardsInput+;
+#pragma link C++ class NaMixture+;
+#pragma link C++ class NaMaterial+;
+#pragma link C++ class BeamPipe+;
+#pragma link C++ class MagField+;
+
+#endif
